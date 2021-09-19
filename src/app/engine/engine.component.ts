9ea558d2411008862858c4f7093e15d0fb827f7e
Dilan Fernando <spegusess@gmail.com>
2021-09-19 21:16:53 +0530

    added working 3D model support

--- src/app/engine/engine.component.ts
+++ src/app/engine/engine.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { AmbientLight, PointLight } from 'three';
 
 
 @Component({
@@ -13,37 +14,78 @@ export class EngineComponent implements OnInit {
 
   constructor() {
   }
-  //@ViewChild('rendererContainer', null) rendererCanvas: ElementRef;
+
+  //@ViewChild('rendererContainer') canvas: ElementRef;
+  //canvas: ElementRef<HTMLCanvasElement>;
+  @ViewChild('rendererContainer') canvas: ElementRef<HTMLCanvasElement>;
+
   ngOnInit(): void {
 
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, 580 / 480, 0.1, 1000);
-    //const rendererContainer: HTMLCanvasElement;
+    const camera = new THREE.PerspectiveCamera(12, 577 / 488, 0.1, 1000);
+    //const canvas = document.getElementById("rendererContainer");
 
     const renderer = new THREE.WebGLRenderer({
+      //canvas: canvas,
       alpha: true,    // transparent background
       antialias: true // smooth edges
     });
-    renderer.setSize(580, 480);
-    //this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
-    //document.getElementByClass('engine-wrapper').appendChild(renderer.domElement)
-    document.body.appendChild(renderer.domElement);
-    //rendererContainer.appendChild(renderer.domElement);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(577, 488);
+
+    document.body.appendChild(renderer.domElement); //render content on page
+    //this.canvas.nativeElement(renderer.domElement);
+    //this.canvas.nativeElement.querySelector('#rendererContainer');
 
     const loader = new GLTFLoader();
     loader.load('assets/Models/Shirt01.glb', function (gltf) {
       scene.add(gltf.scene);
+      //const model = gltf.scene.children[0];
+      const model = gltf.scene;
+      model.position.set(0, -1.3, 0)
+
     }, undefined, function (error) {
       console.error(error);
     });
 
-    camera.position.z = 3;
+    camera.position.z = 4;
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+    const pointLight = new THREE.PointLight(0xffffff)
+    pointLight.position.set(15, 15, 15)
+
+    const ambientLight = new THREE.AmbientLight(0xffffff)
+    scene.add(pointLight, ambientLight)
+
+    //const lightHelper = new THREE.PointLightHelper(pointLight);
+    //const gridHelper = new THREE.GridHelper(200, 50)
+    //scene.add(lightHelper, gridHelper)
 
     function animate() {
       requestAnimationFrame(animate);
       renderer.render(scene, camera);
+
+      controls.update();
     }
     animate();
   }
-
 }
+
+//@ViewChild('canvas', { static: true })
+      //@ViewChild('rendererContainer', null) rendererContainer: ElementRef;
+      //@ViewChild('rendererContainer', null) canvas: ElementRef;
+      //@ViewChild('rendererContainer', { static: true })
+      //canvas: ElementRef<HTMLCanvasElement>;
+    //private ctx: CanvasRenderingContext2D;
+    //this.ctx = this.canvas.nativeElement.getContext('2d');
+    //const canvas = new ElementRef<HTMLCanvasElement>;
+    //const canvas: ElementRef<HTMLCanvasElement> = document.querySelector('#rendererContainer');
+    //const canvas: ElementRef<HTMLCanvasElement> = document.body.appendChild(renderer.domElement);
+      //canvas: document.querySelector('#rendererContainer'),
+      //canvas: this.canvas.nativeElement.appendChild('#rendererContainer'),
+    //@ViewChild('rendererContainer') renderContainer: ElementRef<HTMLElement>;
+    //this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
+    //document.getElementsByClassName('engine-wrapper').appendChild(renderer.domElement)
+    //rendererContainer.appendChild(renderer.domElement);
+    //getElementsById('rendererContainer').appendChild(renderer.domElement);
+    //this.canvas.nativeElement.appendChild('#rendererContainer'),
